7e700f925981b43400290c99b22084f26b88f257
sii <sii_side@yahoo.co.jp>
2017-05-26 13:49:54 +0900

    v0.0.1

---
+++ extension-digital-watch/src/js/background.js
@@ -0,0 +1,17 @@
+new Promise(function (resolve) {
+  chrome.runtime.onInstalled.addListener(resolve);
+}).then(function () {
+  return new Promise(function (resolve) {
+    chrome.declarativeContent.onPageChanged.removeRules(undefined, resolve);
+  });
+}).then(function () {
+  chrome.declarativeContent.onPageChanged.addRules([{
+    conditions: [new chrome.declarativeContent.PageStateMatcher({
+      pageUrl: {
+        hostEquals: 'twitter.com',
+        schemes: ['https']
+      }
+    })],
+    actions: [new chrome.declarativeContent.ShowPageAction()]
+  }]);
+});
---
+++ extension-digital-watch/src/js/content.js
@@ -0,0 +1,14 @@
+var watch = document.createElement('div');
+watch.classList.add('ext-watch');
+document.body.appendChild(watch);
+
+var timer = window.setInterval(function() {
+  var now = new Date();
+  var h = now.getHours();
+  h = ('0' + h).slice(-2);
+  var m = now.getMinutes();
+  m = ('0' + m).slice(-2);
+  var s = now.getSeconds();
+  s = ('0' + s).slice(-2);
+  watch.innerHTML = h + ':' + m + ':' + s;
+}, 1000);
